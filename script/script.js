cc762d8e277d5312b8ccf574a32c3d61393b6db2
20minhajj <harith20@icloud.com>
2019-05-11 19:12:01 +0300

    file

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const Sidebar = document.getElementById('sidebar');
+
+
+function toggleSidebar() {
+    Sidebar.classList.toggle('active');  
+}
+
+// function toggleSidebar(){
+//     document.getElementById("sidebar").classList.toggle('active');
+// }   
