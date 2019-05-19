c4acfaa7faec325b93eb0e51543325473bb859a1
20minhajj <harith20@icloud.com>
2019-05-19 14:51:11 +0300

    feat: all table of the app
    this table provide various decription concern the page which you are in
    
    see #14

---
+++ Front-end/src/components/tables/rides-taken.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../tables.table.css';
+
+class RideTaken extends Component{
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default RideTaken;
