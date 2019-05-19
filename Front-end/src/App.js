ddc872eb4da7b2f0b5e506b526ff6929fb6e2fc9
20minhajj <harith20@icloud.com>
2019-05-19 15:04:01 +0300

    feat: routing the compionents to the components
    Please enter the commit message for your changes. Lines starting
    
    see #19

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/header" 
+import IndexPage from './components/index-page';
+// import IndexButton from './components/Index-button';
+import ButtonIndex from './components/Index-button/button';
+// import RiderSignup from './components/rider-signup/r-signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header />
+      <IndexPage />
+      <ButtonIndex />
+      {/* <RiderSignup /> */}
+      
+
     </div>
   );
 }
